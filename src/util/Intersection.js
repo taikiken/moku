0ac845e69eb76ab44c6eb57238999504eb3f2199
taikiken <denkosekka@gmail.com>
2020-03-12 19:49:33 +0900

    update - Intersection docs

--- src/util/Intersection.js
+++ src/util/Intersection.js
@@ -6,12 +6,24 @@ import 'intersection-observer';
  * - 画面縦幅より大きな `HTMLElement` には反応しないので注意します
  */
 export default class Intersection {
+  /**
+   * `IntersectionObserver` callback
+   * - `isIntersecting` property を使用し hit しているかを判定します
+   * @param {Array<IntersectionObserverEntry>} entries `IntersectionObserverEntry` list
+   * @see https://wiki.developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API#Intersection_observer_concepts_and_usage
+   * @see https://wiki.developer.mozilla.org/en-US/docs/Web/API/IntersectionObserverEntry
+   */
   check = (entries) => {
     entries.map(entry => (
       entry.isIntersecting ? this.intersect(entry) : this.parallel(entry)
     ))
   }
 
+  /**
+   * `IntersectionObserver` 準備します
+   * @param {Array<HTMLElement>} elements target Element list
+   * @param {{root: ?HTMLElement, rootMargin: string, threshold: Array<number>}} options `IntersectionObserver` option
+   */
   constructor(
     elements,
     options = {
@@ -20,7 +32,15 @@ export default class Intersection {
       threshold: [0.5],
     },
   ) {
+    /**
+     * target Element list
+     * @type {Array<HTMLElement>}
+     */
     this.elements = elements
+    /**
+     * `IntersectionObserver` instance
+     * @type {IntersectionObserver}
+     */
     this.observer = new IntersectionObserver(this.check, options);
   }
 
